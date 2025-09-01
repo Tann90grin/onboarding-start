fb27f15ecd898f755ec439e7b3501059f7bb311c
Henry Wu <h27wu@uwaterloo.ca>
2025-09-01 16:10:52 -0400

    fix

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -42,11 +42,11 @@ always @(posedge clk or negedge rst_n) begin
         bit_cnt <= 5'b0;
         trans_comp <= 1'b0;
     end else begin
-        if(ncs_negedge && ~trans_comp) begin
+        if(ncs_negedge) begin
             spi_buf <= 16'b0;
             bit_cnt <= 5'b0;
             trans_comp <= 1'b0;
-        end else if(ncs_sync == 2'b0 && bit_cnt < 5'd16 && ~trans_comp) begin
+        end else if(ncs_sync[1] == 1'b0 && bit_cnt < 5'd16) begin
             if(sclk_posedge) begin
                 spi_buf <= {spi_buf[14:0], copi_sync[1]};
                 bit_cnt <= bit_cnt + 1;
