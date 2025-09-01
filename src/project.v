0ecbd86cd86a33722a4865cbd7ffaf832f7070a5
Henry Wu <h27wu@uwaterloo.ca>
2025-09-01 16:04:21 -0400

    fix

--- src/project.v
+++ src/project.v
@@ -28,7 +28,7 @@ module tt_um_uwasic_onboarding_henry_wu (
   spi_peripheral spi_peripheral_inst (
     .clk(clk),
     .rst_n(rst_n),
-    .ui_in(ui_in[2:0]).
+    .ui_in(ui_in[2:0]),
     .en_reg_out_7_0(en_reg_out_7_0),
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
