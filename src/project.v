bc98f5ff331b77ca1fc05f4986b63e40ca51a4fe
Henry Wu <h27wu@uwaterloo.ca>
2025-09-01 16:04:53 -0400

    fix

--- src/project.v
+++ src/project.v
@@ -33,7 +33,7 @@ module tt_um_uwasic_onboarding_henry_wu (
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
-    .pwm_duty_cycle(pwm_duty_cycle),
+    .pwm_duty_cycle(pwm_duty_cycle)
   );
 
   // Instantiate the PWM module
