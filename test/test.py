aa55c6f023da2b092ec23d7b110c2099b9cac15e
Tann90grin <h27wu@uwaterloo.ca>
2025-09-01 23:22:21 -0400

    pwm tests

--- test/test.py
+++ test/test.py
@@ -3,8 +3,9 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge
+from cocotb.triggers import RisingEdge, FallingEdge
 from cocotb.triggers import ClockCycles
+from cocotb.utils import get_sim_time
 from cocotb.types import Logic
 from cocotb.types import LogicArray
 
@@ -83,6 +84,25 @@ async def send_spi_transaction(dut, r_w, address, data):
     await ClockCycles(dut.clk, 600)
     return ui_in_logicarray(ncs, bit, sclk)
 
+async def posedge_detect(dut, cycles = 10000):
+    posedge_tim = -1
+    for _ in range(cycles):
+        await ClockCycles(dut.clk, 1)
+        if dut.uo_out.value.integer & 1 == 1:
+            posedge_tim = get_sim_time('ns')
+            break
+    return posedge_tim
+
+async def negedge_detect(dut, cycles = 10000):
+    negedge_tim = -1
+    for _ in range(cycles):
+        await ClockCycles(dut.clk, 1)
+        if dut.uo_out.value.integer & 1 == 0:
+            negedge_tim = get_sim_time('ns')
+            break
+    return negedge_tim
+        
+
 @cocotb.test()
 async def test_spi(dut):
     dut._log.info("Start SPI test")
@@ -152,10 +172,103 @@ async def test_spi(dut):
 @cocotb.test()
 async def test_pwm_freq(dut):
     # Write your test here
+    dut._log.info("Start PWM Frequency test")
+
+    # Set the clock period to 100 ns (10 MHz)
+    clock = Clock(dut.clk, 100, units="ns")
+    cocotb.start_soon(clock.start())
+    
+    # Reset
+    dut._log.info("Reset")
+    dut.ena.value = 1
+    ncs = 1
+    bit = 0
+    sclk = 0
+    dut.ui_in.value = ui_in_logicarray(ncs, bit, sclk)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 5)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 5)
+
+    await send_spi_transaction(dut, 1, 0x00, 0xFF)
+    await send_spi_transaction(dut, 1, 0x01, 0xFF)
+    await send_spi_transaction(dut, 1, 0x02, 0xFF)
+    await send_spi_transaction(dut, 1, 0x03, 0xFF)
+    #50% duty cycle for test
+    await send_spi_transaction(dut, 1, 0x04, 0x80)
+
+    dut._log.info(f"Frequency Test")
+
+    #Synchronization
+    await posedge_detect(dut)
+    await negedge_detect(dut)
+    await posedge_detect(dut)
+
+    posedge1_tim = await posedge_detect(dut)
+    negedge_tim = await negedge_detect(dut)
+    posedge2_tim = await posedge_detect(dut)
+    freq = 1e9 / (posedge2_tim - posedge1_tim)
+    assert 2970 <= freq <= 3030, f"Expected 3kHz, got {freq:.2f}"
+    dut._log.info(f"Actual Frequency = {freq:.2f}")
+
     dut._log.info("PWM Frequency test completed successfully")
 
 
 @cocotb.test()
 async def test_pwm_duty(dut):
     # Write your test here
+    dut._log.info("Start PWM Frequency test")
+
+    # Set the clock period to 100 ns (10 MHz)
+    clock = Clock(dut.clk, 100, units="ns")
+    cocotb.start_soon(clock.start())
+    
+    # Reset
+    dut._log.info("Reset")
+    dut.ena.value = 1
+    ncs = 1
+    bit = 0
+    sclk = 0
+    dut.ui_in.value = ui_in_logicarray(ncs, bit, sclk)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 5)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 5)
+
+    await send_spi_transaction(dut, 1, 0x00, 0xFF)
+    await send_spi_transaction(dut, 1, 0x01, 0xFF)
+    await send_spi_transaction(dut, 1, 0x02, 0xFF)
+    await send_spi_transaction(dut, 1, 0x03, 0xFF)
+
+    # 0% Duty Cycle
+    await send_spi_transaction(dut, 1, 0x04, 0x00)
+    dut._log.info(f"Test Duty Cycle: 0%")
+    posedge1_tim = await posedge_detect(dut)
+    assert posedge1_tim < 0, f"Expected 0%"
+    dut._log.info(f"Actual Duty cycle = 0.00 %")
+
+    # 50% Duty Cycle
+    await send_spi_transaction(dut, 1, 0x04, 0x80)
+    dut._log.info(f"Duty Cycle: 50%")
+
+    #Synchronization
+    await posedge_detect(dut)
+    await negedge_detect(dut)
+    await posedge_detect(dut)
+
+    posedge1_tim = await posedge_detect(dut)
+    negedge_tim = await negedge_detect(dut)
+    posedge2_tim = await posedge_detect(dut)
+    duty_cycle = (negedge_tim - posedge1_tim) * 100 / (posedge2_tim - posedge1_tim)
+    assert 49.90 < duty_cycle < 50.10, f"Expected 0%"
+    dut._log.info(f"Actual Duty cycle = {duty_cycle:.2f} %")
+
+    # 100% Duty Cycle
+    await send_spi_transaction(dut, 1, 0x04, 0xFF)
+    dut._log.info(f"Duty Cycle: 100%")
+    posedge1_tim = await posedge_detect(dut)
+    negedge_tim = await negedge_detect(dut)
+    assert negedge_tim < 0, f"Expected 100%"
+    dut._log.info(f"Actual Duty cycle = 100.00 %")
+
     dut._log.info("PWM Duty Cycle test completed successfully")
